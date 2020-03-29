08a8d93738d029d63c4af94361a476a6844e4f29
Dominik Riedig <me@dominikriedig.de>
2020-03-29 15:24:37 +0200

    WIP: object-oriented theme-changing via console, but overriding doesnt work yet

--- src/js/modules/rescheme.js
+++ src/js/modules/rescheme.js
@@ -2,7 +2,24 @@ class Rescheme {
   constructor(arg1, arg2, arg3) {
     const element = arg1, preset = arg2, config = arg3
     console.log(element, preset, config);
-    document.getElementById
+  }
+
+  changeTheme(arg1) {
+    const newtheme = arg1
+    document.querySelectorAll('*').forEach(function(node) {
+      var classes = node.className
+      var clist = classes.split(/\s+/)
+      if(clist.length > 0) console.log(node + " has classes " + clist)
+      for(var i = clist.length-1; 0; i-1) {
+        if(clist[i].startsWith("force-theme-")) {
+          console.log("Removing class " + clist[i] + " from " + node);
+          // TODO: Removing doesnt work yet. HUH?
+          clist = clist.splice(i, 1);
+        }
+      }
+      clist.push(newtheme)
+      node.className= clist.join(" ")
+    })
   }
 }
 
