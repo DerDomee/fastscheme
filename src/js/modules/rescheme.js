9cebc023f2a84841f02ad18e4e450a62ef60a5f4
Dominik Riedig <me@dominikriedig.de>
2020-03-29 23:15:37 +0200

    Old functionality for theme changer is now restored, but object-oriented

--- src/js/modules/rescheme.js
+++ src/js/modules/rescheme.js
@@ -1,7 +1,30 @@
+
+const DEFAULT_THEMES = [{name: "Use Browser Default", class: ""},
+                        {name: "Light Theme", class: "force-theme-light"},
+                        {name: "Dark Theme", class: "force-theme-dark"},
+                        {name: "Black Theme", class: "force-theme-darkest"}
+                      ]
+
 class Rescheme {
-  constructor(arg1, arg2, arg3) {
-    const element = arg1, preset = arg2, config = arg3
-    console.log(element, preset, config);
+
+
+  constructor(arg1, arg2) {
+    const element = arg1, config = arg2
+    var selector = document.createElement('select')
+    element.appendChild(selector)
+    if(config === null) {
+      DEFAULT_THEMES.forEach(function(theme) {
+        var option = document.createElement('option')
+        option.appendChild(document.createTextNode(theme.name))
+        option.setAttribute('theme-class', theme.class)
+        selector.appendChild(option)
+      })
+    selector.addEventListener("change", () => {
+      var selIndex = selector.options[selector.selectedIndex]
+      var indexClass = selIndex.getAttribute('theme-class')
+      this.changeTheme(indexClass)
+    })
+    }
   }
 
   /**
@@ -19,6 +42,8 @@ class Rescheme {
     })
   }
 }
+var _defaultdomelement = document.querySelectorAll("scheme-select")[0]
+new Rescheme(_defaultdomelement, null)
 
 export {
   Rescheme
