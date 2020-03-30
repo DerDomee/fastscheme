cf0e7e348dedca0b5d3f7e69a7aee9b77592cd2b
Dominik Riedig <me@dominikriedig.de>
2020-03-30 11:57:38 +0200

    Splitting big and dirty constructor into multiple functions

--- src/js/modules/rescheme.js
+++ src/js/modules/rescheme.js
@@ -5,29 +5,75 @@ const DEFAULT_THEMES = [
   {name: "Dark Theme", class: "force-theme-dark"},
   {name: "Black Theme", class: "force-theme-darkest"}]
 
+/**
+  * Rescheme Class providing the selector for scheme-overrides.
+  * The function of @prefers-color-scheme works without any javascript,
+  * this class only exists for manual overriding of the theme by the user.
+  *
+  *
+  */
 class Rescheme {
 
-
+  /**
+    * Generate a theme-selector and registering
+    * the corresponding event listener.
+    * @constructor
+    * @param {DOMElement} arg1 element: The DOMElement where the selector should be placed in
+    * @param {Object[]} [arg2] config: Custom Configuration of theme names and classes
+    * @param {String} arg2[].name - The displayed name of the theme
+    * @param {String} arg2[].class - The class utilizing this theme
+    * @throws InvalidOperationException - when arg1 is not a valid element in the DOM
+    */
   constructor(arg1, arg2) {
     const element = arg1, config = arg2
+    if(element === null || !this._isDomElement(element)) {
+      throw new Error("IllegalArgument - arg1 is not a DOM element")
+    }
+    if(config === null) {
+      this._setupDefaultSelector(element)
+    } else {
+      this._setupWithConfiguration(element, config)
+    }
+  }
+
+  _setupDefaultSelector(arg1) {
+    const element = arg1
     var selector = document.createElement('select')
     element.appendChild(selector)
-    if(config === null) {
-      DEFAULT_THEMES.forEach(function(theme) {
-        var option = document.createElement('option')
-        option.appendChild(document.createTextNode(theme.name))
-        option.setAttribute('theme-class', theme.class)
-        selector.appendChild(option)
-      })
+    DEFAULT_THEMES.forEach(function(theme) {
+      var option = document.createElement('option')
+      option.appendChild(document.createTextNode(theme.name))
+      option.setAttribute('theme-class', theme.class)
+      selector.appendChild(option)
+    })
     selector.addEventListener("change", () => {
       var selIndex = selector.options[selector.selectedIndex]
       var indexClass = selIndex.getAttribute('theme-class')
       this.changeTheme(indexClass)
     })
+  }
+
+  _setupWithConfiguration(arg1, arg2) {
+    const element = arg1, config = arg2
+    throw new Error("_setupWithConfiguration is not supported " +
+                    "in this Version yet!");
+  }
+
+  //TODO: Move this function into new js file DomUtils
+  _isDomElement(arg1) {
+    const element = arg1
+    try {
+      return element instanceof HTMLElement
+    } catch(e) {
+      return (typeof element === "object") && (element.nodeType === 1) && (typeof element.style === "object") && (typeof element.ownerDocument === "object")
     }
   }
 
   /**
+    * Remove every 'force-theme-*' class from every element in the DOM,
+    * then add none or one new 'force-theme-*' class to toggle a theme.
+    *
+    * @param {string} arg1 newtheme: Name of the new theme-class to add. Use null or empty string to reset the theme.
     */
   changeTheme(arg1, arg2 = true) {
     const newtheme = arg1, usecookie = arg2
