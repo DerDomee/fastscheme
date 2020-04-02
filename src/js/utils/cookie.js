c85df5eb3489fb787a9ae33d06cb589b4cf26c65
Dominik Riedig <me@dominikriedig.de>
2020-04-02 16:49:01 +0200

    Add cookie util functions

---
+++ src/js/utils/cookie.js
@@ -0,0 +1,29 @@
+const set_cookie = function (arg1, arg2, arg3) {
+  const key = arg1, value = arg2, expiry = arg3
+  var d = new Date();
+  d.setTime(d.getTime() + (expiry*24*60*60*1000));
+  var expires = "expires="+ d.toUTCString();
+  document.cookie = key + "=" + value + ";" + expiry + ";path=/";
+}
+
+const get_cookie = function (arg1) {
+  const key = arg1
+  var name = key + "=";
+    var decodedCookie = decodeURIComponent(document.cookie);
+    var ca = decodedCookie.split(';');
+    for(var i = 0; i <ca.length; i++) {
+      var c = ca[i];
+      while (c.charAt(0) == ' ') {
+        c = c.substring(1);
+      }
+      if (c.indexOf(name) == 0) {
+        return c.substring(name.length, c.length);
+      }
+    }
+    return "";
+}
+
+export {
+  set_cookie,
+  get_cookie
+}
